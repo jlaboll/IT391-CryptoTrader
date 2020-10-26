77225336ce98f5af0467113c6292a762a0f671e9
Ryan <rcbarr@ilstu.edu>
2020-10-26 04:11:09 -0700

    Removed the ability to sell into negatives Fixed a bug where cancelling the prompt breaks the wallet

--- crypto-trader/src/Wallet.js
+++ crypto-trader/src/Wallet.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import NumberFormat from "react-number-format";
 
 //TODO: Fix handleSell to not let negatives
-//TODO: Style component
 
 export default class Wallet extends Component {
     state = {
@@ -11,8 +10,8 @@ export default class Wallet extends Component {
 
     };
     promptAmt(){
-        var amount = prompt("How much?", 1);
-        if(isNaN(amount)){return 0;} else {
+        let amount = prompt("How much?", 1);
+        if(amount===null){return 0;} else {
             return amount;
         }
     }
@@ -25,8 +24,12 @@ export default class Wallet extends Component {
     handleSell = () => {
         let sellAmount = this.promptAmt();
         let value = parseFloat(this.state.count);
-        let newValue = value - parseFloat(sellAmount);
-        this.setState({count: newValue});
+        if(value == 0 && sellAmount!= 0){alert("No coins to sell");} else {
+            let newValue = value - parseFloat(sellAmount);
+            if(newValue < 0){alert("You don't have that much");}
+            else{this.setState({count: newValue});}
+
+        }
     }
     render(){
         // console.log('props', this.props);
@@ -45,7 +48,8 @@ export default class Wallet extends Component {
     }
 
     displayCount(){
-        return this.state.count
+        let count = this.state.count;
+        return count;
     }
     calculateAmount(){
         // coinValue * count
